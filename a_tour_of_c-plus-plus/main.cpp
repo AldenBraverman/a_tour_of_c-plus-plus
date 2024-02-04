fb00eda049f4f9ae68a910d84715a7fafca366a5
AldenBraverman <aldenbrave@aol.com>
2024-02-04 13:27:32 -0500

    Initial Commit

---
+++ a_tour_of_c-plus-plus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  a_tour_of_c-plus-plus
+//
+//  Created by Alden Braverman on 2/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
