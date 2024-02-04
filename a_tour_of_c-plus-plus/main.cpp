c8e5d7508d9e1a52e0db307802f025cf8959eb63
AldenBraverman <aldenbrave@aol.com>
2024-02-04 18:06:57 -0500

    update
    up to 1.3 functions

--- a_tour_of_c-plus-plus/main.cpp
+++ a_tour_of_c-plus-plus/main.cpp
@@ -4,11 +4,34 @@
 //
 //  Created by Alden Braverman on 2/4/24.
 //
+/*
+ #include <iostream>
+ 
+ int main(int argc, const char * argv[]) {
+ // insert code here...
+ std::cout << "Hello, World!\n";
+ return 0;
+ }
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+double square(double x)
+{
+    return x*x;
+}
+
+void print_square(double x)
+{
+    std::cout << "the square of " << x << " is " << square(x) << "\n";
+}
+
+int main()
+{
+    // std::cout << "Hello, World!\n";
+    /*
+     << operator writes its second argument onto its first
+     string literal "Hello World!\n" is written onto the standard output stream std::cout
+     */
+    print_square(1.234);
 }
